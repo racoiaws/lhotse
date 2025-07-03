0f03155e6b3ba67d01f7fa850be805be77ac4280
Roman Korostik <rkorostik@nvidia.com>
2025-07-03 22:13:17 +0400

    Lowpass: set resample backend globally ('default' or 'sox' for now)

--- lhotse/audio/recording.py
+++ lhotse/audio/recording.py
@@ -890,7 +890,7 @@ class Recording:
             transforms=transforms,
         )
 
-    def resample(self, sampling_rate: int, backend: str = "default") -> "Recording":
+    def resample(self, sampling_rate: int) -> "Recording":
         """
         Return a new ``Recording`` that will be lazily resampled while loading audio.
         :param sampling_rate: The new sampling rate.
@@ -904,7 +904,6 @@ class Recording:
             Resample(
                 source_sampling_rate=self.sampling_rate,
                 target_sampling_rate=sampling_rate,
-                backend=backend,
             )
         )
 
